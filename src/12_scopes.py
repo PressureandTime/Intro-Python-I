1e79ab4b7cd729626444ca5fc879709be242afaa
Petar Scepanovic <hiborija@gmail.com>
2019-10-01 19:01:05 +0200

    finished files.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -6,8 +6,9 @@ x = 12
 
 def changeX():
     x = 99
+    return x
 
-changeX()
+print(changeX())
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
 print(x)
@@ -19,12 +20,13 @@ def outer():
     y = 120
 
     def inner():
-        y = 999
+       y = print(999)
 
-    inner()
+
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
     print(y)
+    return inner()
 
 outer()
